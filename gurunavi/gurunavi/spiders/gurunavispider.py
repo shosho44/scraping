a3b24b3821e20efbeec115aa6e19f10edb2875b4
ssshotaro44 <shotaro2878@gmail.com>
2021-03-21 23:39:36 +0900

    [gurunavi] edit allowed_domains and start_urls

--- gurunavi/gurunavi/spiders/gurunavispider.py
+++ gurunavi/gurunavi/spiders/gurunavispider.py
@@ -3,8 +3,9 @@ import scrapy
 
 class GurunavispiderSpider(scrapy.Spider):
     name = 'gurunavispider'
-    allowed_domains = ['https://r.gnavi.co.jp/area/jp/rs/?fwr=%E3%83%99%E3%82%B8%E3%82%BF%E3%83%AA%E3%82%A2%E3%83%B3']
-    start_urls = ['http://https://r.gnavi.co.jp/area/jp/rs/?fwr=%E3%83%99%E3%82%B8%E3%82%BF%E3%83%AA%E3%82%A2%E3%83%B3/']
+    allowed_domains = ['r.gnavi.co.jp']
+    start_urls = ['http://https://r.gnavi.co.jp/area/jp/rs/?fwr=%E3%83%99%E3%82%B8%E3%82%BF%E3%83%AA%E3%82%A2%E3%83%B3/',
+                  'https://r.gnavi.co.jp/area/jp/rs/?fwr=%E3%83%B4%E3%82%A3%E3%83%BC%E3%82%AC%E3%83%B3']
 
     def parse(self, response):
-        pass
+        
