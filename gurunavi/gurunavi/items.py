c6868e4bf0e9e77572e4742bcd75798aaf4c847d
ssshotaro44 <shotaro2878@gmail.com>
2021-03-21 23:33:21 +0900

    write items.pyのフィールド作成

--- gurunavi/gurunavi/items.py
+++ gurunavi/gurunavi/items.py
@@ -8,5 +8,13 @@ import scrapy
 
 class GurunaviItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
+    store_name = scrapy.Field()  # 店名
+    phone_num = scrapy.Field()  # 電話番号
+    postal_code = scrapy.Field()  # 郵便番号
+    location = scrapy.Field()  # 場所
+    opening_hours = scrapy.Field()  # 営業時間
+    regular_holiday = scrapy.Field()  # 定休日
+    gurunavi_url = scrapy.Field()  # ぐるなびのurl
+    genre = scrapy.Field()  # ジャンル
+    key_word = scrapy.Field()
     pass
