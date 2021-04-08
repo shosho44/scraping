927bd77a016e3315eb2109cb6f3f2fd11a9d102a
ssshotaro44 <shotaro2878@gmail.com>
2021-04-09 01:18:51 +0900

    itemフィールドを作った

--- tabelog/tabelog/items.py
+++ tabelog/tabelog/items.py
@@ -7,6 +7,13 @@ import scrapy
 
 
 class TabelogItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    store_url_of_tabelog = scrapy.Field()  # 店舗の食べログurl
+    location = scrapy.Field()  # 店舗住所
+    store_name = scrapy.Field()  # 店名
+    genre = scrapy.Field()  # 食べ物のジャンル
+    phone_number = scrapy.Field()  # 電話番号
+    store_homepage = scrapy.Field()  # 店舗のホームページ
+    reservation_availability = scrapy.Field()  # 予約可能かどうか
+    transportaton = scrapy.Field()  # 交通手段
+    store_score = scrapy.Field()  # 星評価数
+    budget = scrapy.Field()  # 予算
