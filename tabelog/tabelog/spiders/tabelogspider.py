f55b636c65d8962fa57af96600ffdd1ee6f42372
ssshotaro44 <shotaro2878@gmail.com>
2021-04-09 21:35:02 +0900

    店舗のページから情報を取れるようにした

--- tabelog/tabelog/spiders/tabelogspider.py
+++ tabelog/tabelog/spiders/tabelogspider.py
@@ -18,4 +18,38 @@ class TabelogspiderSpider(scrapy.Spider):
             yield scrapy.Request(next_page_url, self.parse)
     
     def get_info(self, response):
-        pass
+        item['store_url_of_tabelog'] = response.url
+        store_score = response.css('#js-detail-score-open > p > b > span::text').get()
+        item['score_score'] = store_score
+        is_phone_number = response.css('#column-side > div > div > div > div > div > div > div > p::text').get()
+        if is_phone_number is not None:
+            phone_number = is_phone_number
+            item['phone_number'] = phone_number
+        
+        table_tr_selectors = response.css('#rst-data-head > table > tbody > tr')
+        for table_tr_selector in table_tr_selectors:
+            th_content = ''.join(table_tr_selector.css('th ::text').getall())
+            td_content = ''.join(table_td_selector.css('td ::text').getall())
+            if '店名' in th_content:
+                store_name = td_content
+                item['store_name'] = store_name
+            if 'ジャンル' in th_content:
+                genre = td_content
+                item['genre'] = genre
+            if '予約可否' in th_content:
+                reservation_availability = td_content
+                item['reservation_availability'] = reservation_availability
+            if '住所' in th_content:
+                location = td_content
+                item['location'] = location
+            if '交通手段' in th_content:
+                transportaton = td_content
+                item['transportation'] = transportaton
+            if 'ホームページ' in th_content:
+                store_homepage = td_content
+                item['store_homepage'] = store_homepage
+            if '予算' == th_content:
+                budget = td_content
+                item['budget'] = budget
+
+        yield item
